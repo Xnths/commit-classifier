b07dd025af89debb88762e344d3e1187536cf9ea
Xnths <jonathascastilhoes@gmail.com>
2026-01-31 09:35:51 -0300

    feat: adding setup script and error prevention in case there are modules missing
    Because user should not have to remember the commands to run the code

--- nlp.py
+++ nlp.py
@@ -1,5 +1,13 @@
 import spacy
 
+try:
+    spacy.load("en_core_web_sm")
+except OSError:
+    raise RuntimeError(
+        "spaCy model not found. Run: python -m spacy download en_core_web_sm"
+    )
+
+
 nlp = spacy.load("en_core_web_sm")
 doc = nlp("I love designing and building software")
 
---
+++ scripts/setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+poetry install
+poetry run python -m spacy download en_core_web_sm
